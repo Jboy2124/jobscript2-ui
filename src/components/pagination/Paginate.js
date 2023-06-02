1767d9b3a7db074bd07cfe4e9fe3ff7abdd0b34d
Jufel Abecia <jufelabecia@gmail.com>
2023-06-03 00:12:58 +0800

    refactor: finalizing the pagination

---
+++ src/components/pagination/Paginate.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactPaginate from 'react-paginate'
+
+const Paginate = (props) => {
+
+    let totalPage = (isNaN(props.totalPages) ? 0 : props.totalPages)
+
+    const getCurrentPage = (currentPage) => {
+        props.handleSelectedPageClick(currentPage.selected)
+    }
+
+  return (
+    <div className='flex justify-center items-center'>
+        <ReactPaginate 
+             previousLabel={ 'Prev' }
+             nextLabel={'Next'}
+             breakLabel={'...'}
+             pageCount={totalPage}
+             pageRangeDisplayed={4}
+             marginPagesDisplayed={5}
+             onPageChange={getCurrentPage}
+             // containerClassName={'pb-10'}
+             pageClassName={'flex justify-center items-center text-white text-[14px] font-poppins rounded hover:bg-orange-700 duration-200 bg-orange-600 px-2 w-10 h-9'}
+             // pageLinkClassName={'text-red-700'} // number per page
+             // breakClassName={'flex justify-center items-center text-white text-[14px] font-poppins rounded hover:bg-orange-700 duration-200 bg-orange-600 px-2 w-10 h-9'}
+             previousClassName={'font-poppins text-[14px] px-3'}
+             nextClassName={'font-poppins text-[14px] px-3'}
+             activeClassName={'bg-orange-800'}
+             className='flex justify-center items-center space-x-2'
+        />
+    </div>
+  )
+}
+
+export default Paginate
--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -4,37 +4,47 @@ import Footer from '../components/footer/Footer'
 import Searchbar from '../components/search/Searchbar'
 import JobSpecCard from '../components/cards/JobSpecCard'
 import { axiosInstance } from '../config/axios'
+import Paginate from '../components/pagination/Paginate'
+
 
 const Jobs = () => {
-  const [data, setData] = useState([])
-  const [total, setTotal] = useState(0)
-  const [offsetVar, setOffsetVar] = useState(0)
-  const limitVal = 4
+  const[items, setItems] = useState([])
+  const[totalPage, setTotalPage] = useState(0)
+  const itemPerPage = 3
 
-  const queryParams = {
-    limit: limitVal,
-    offset: offsetVar
-  }
+ 
+  async function _fetchData(currentPage){
 
-  async function _loadData(){
+    const queryParams = {
+      limit: itemPerPage,
+      offset: currentPage * itemPerPage
+    }
+
     await axiosInstance({
       method: 'GET',
       url: '/jobs',
       params: queryParams
     })
     .then(response => {
-      setData(host => [...host, ...response.data.jobs])
-      setTotal(response.data.total)
-      setOffsetVar(val => val + limitVal)
+      setItems(response.data.jobs)
+      setTotalPage(response.data.total)
     })
     .catch(error => {
       console.log(error)
     })
   }
 
+
   useEffect(() => {
-    _loadData()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+    _fetchData()
+  }, [])
+
+
+  function handlePageClick(currentPage){
+    _fetchData(currentPage)
+  }
+
+  let TotalPage = Math.ceil((isNaN(totalPage[0]?.totalRows) ? 0 : totalPage[0]?.totalRows) / itemPerPage)
 
  
   return (
@@ -45,7 +55,7 @@ const Jobs = () => {
             <div className='min-h-screen flex justify-center items-center'>
                 <div className='grid grid-cols-2 gap-4 place-items-center py-14'>
                   {
-                    data.map((items, index) => {
+                    items.map((items, index) => {
                       return(
                         <div key={index}>
                           <JobSpecCard 
@@ -61,12 +71,16 @@ const Jobs = () => {
                 </div>
             </div>
             <div className='flex justify-center items-center pb-10'>
-              <button 
+              {/* <button 
                 type='button'
                 onClick={() => _loadData()}
                 className={`${ (offsetVar <= total[0]?.totalRows ) ? 'block' : 'hidden' }  px-32 py-3 bg-orange-600 shadow-lg hover:bg-orange-700 duration-300 rounded text-[15px] text-white font-poppins`}>
                   Load more...
-              </button>
+              </button> */}
+              <Paginate 
+                  totalPages={TotalPage}
+                  handleSelectedPageClick={handlePageClick}
+                />
             </div>
         </div>
         <Footer />
